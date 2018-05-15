fa9c6e6134021ec242d1ae2ce26ce5d7e78bda69
denmasyarikin <denmasyarikin@gmail.com>
2018-05-15 20:28:03 +0700

    [FIX] refactor order item

--- src/InventoryServiceProvider.php
+++ src/InventoryServiceProvider.php
@@ -5,9 +5,20 @@ namespace Denmasyarikin\Inventory;
 use App\Manager\Facades\Package;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class InventoryServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Relation::morphMap(['good_variant' => 'Denmasyarikin\Inventory\Good\GoodVariant']);
+    }
+
     /**
      * Register any application services.
      */
